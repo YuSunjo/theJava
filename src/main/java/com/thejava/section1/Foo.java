22dea6eb62b77526dc8d3ee745bb0f81490f2b20
YuSunjo <tnswh2023@gmail.com>
2023-08-24 11:16:08 +0900

    feat(the java - java8): 메서드 레퍼런스, 디폴트 인터페이스

--- src/main/java/com/thejava/section1/Foo.java
+++ src/main/java/com/thejava/section1/Foo.java
@@ -9,11 +9,11 @@ public class Foo {
      * 1. 함수형 인터페이스
      * 2. 람다 표현식
      * 3. 자바에서 함수형 프로그래밍
-     *    - 함수를 First class object로 사용할 수 있다.
-     *    - 순수 함수(Pure function)
-     *    - 고차 함수(Higher-Order function)
-     *    - 불변성
-     *    link: 스https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
+     * - 함수를 First class object로 사용할 수 있다.
+     * - 순수 함수(Pure function)
+     * - 고차 함수(Higher-Order function)
+     * - 불변성
+     * link: 스https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
      */
     public static void main(String[] args) {
 
